4024f67e2a4392c333ef3cdc29a56d4a03c24cf7
ajgoras <ajgoras@MacBook-Air-ajgoras.local>
2022-10-02 10:48:52 +0200

    added next features-row to infoPage, added interactive moves and timer to infoPage

--- src/infoPageScript.js
+++ src/infoPageScript.js
@@ -1,11 +1,66 @@
-let gameBlocks = document.querySelectorAll('.block');
-const gameField = document.querySelector(".gamePresentation");
+let infoGameBlocks = document.querySelectorAll('.block');
+const infoGameField = document.querySelector(".gamePresentation");
 let isPuzzleSolved = false;
-let gameState = [
-    [gameBlocks[0], gameBlocks[1], gameBlocks[2]],
-    [gameBlocks[3], gameBlocks[4], gameBlocks[5]],
-    [gameBlocks[6], gameBlocks[7], gameBlocks[8]]
+let isTimerRunning = false;
+let countdownTimerInterval = null;
+let timerPresentation = document.getElementById('timerPresentation');
+let infoGameState = [
+    [infoGameBlocks[0], infoGameBlocks[1], infoGameBlocks[2]],
+    [infoGameBlocks[3], infoGameBlocks[4], infoGameBlocks[5]],
+    [infoGameBlocks[6], infoGameBlocks[7], infoGameBlocks[8]]
 ];
+
+let infoMovesCount = 0;
+let interval = null;
+let infoMoves = document.getElementById('moves');
+
+function startIncreasingMoves() {
+    function increaseMoves() {
+        infoMovesCount++;
+        infoMoves.innerText = "Moves: " + infoMovesCount;
+    }
+    function generateRandomNumber() {
+        return 1000 * Math.floor(Math.random() * 150) + 10;
+    }
+
+    function increaseMove() {
+            setTimeout(() => {
+                increaseMoves();
+            }, generateRandomNumber());
+    }
+    async function asyncCall() {
+        for (let i = 0; i < 215; i++) {
+            await increaseMove();
+        }
+    }
+    asyncCall();
+}
+
+let countdownTimer = 0;
+let infoTimer = document.getElementById('timer');
+
+function infoFormatTime(time) {
+    let minutes = Math.floor(time / 60);
+    let seconds = (time % 60).toFixed(0);
+    return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+}
+
+function runCountdown() {
+    countdownTimerInterval = setInterval(() => {
+        countdownTimer++;
+        let formattedCountdownTimer = countdownTimer;
+        formattedCountdownTimer = infoFormatTime(formattedCountdownTimer);
+        infoTimer.innerText = 'Time: ' + formattedCountdownTimer;
+    }, 1000);
+    stopIntervalAfterTime();
+}
+
+function stopIntervalAfterTime() {
+    setTimeout(() => {
+        clearInterval(countdownTimerInterval);
+        countdownTimerInterval=null;
+    }, 1200000);
+}
 
 
 function applyWin() {
@@ -20,22 +75,22 @@ function applyWin() {
 
 function solvePuzzle() {
     setTimeout(() => {
-        gameBlocks[7].style.top = '200px'
+        infoGameBlocks[7].style.top = '200px'
     }, 600);
     setTimeout(() => {
-        gameBlocks[5].style.top = '100px'
+        infoGameBlocks[5].style.top = '100px'
     }, 800);
     setTimeout(() => {
-        gameBlocks[2].style.left = '200px'
+        infoGameBlocks[2].style.left = '200px'
     }, 1000);
     setTimeout(() => {
-        gameBlocks[1].style.top = '0px'
+        infoGameBlocks[1].style.top = '0px'
     }, 1200);
     setTimeout(() => {
-        gameBlocks[4].style.top = '100px'
+        infoGameBlocks[4].style.top = '100px'
     }, 1400);
     setTimeout(() => {
-        gameBlocks[7].style.left = '100px' 
+        infoGameBlocks[7].style.left = '100px' 
     }, 1600);
     setTimeout(() => {
         applyWin();
@@ -44,35 +99,35 @@ function solvePuzzle() {
 }
 
 
-function renderMainPresentation(gameState) {
+function renderMainPresentation(infoGameState) {
     px = 100;
-    gameState.forEach((rowElement, rowIndex) => {
+    infoGameState.forEach((rowElement, rowIndex) => {
         rowElement.forEach((columnElement, columnIndex) => {
             columnElement.style['background-position-y'] = `-${rowIndex * px}px`
             columnElement.style['background-position-x'] = `-${columnIndex * px}px`
         })
     })
 
-    gameBlocks[0].style.top =  '0px'
-    gameBlocks[0].style.left = '0px'
-    gameBlocks[1].style.top =  '100px'
-    gameBlocks[1].style.left = '100px'
-    gameBlocks[2].style.top =  '0px'
-    gameBlocks[2].style.left = '100px'
+    infoGameBlocks[0].style.top =  '0px'
+    infoGameBlocks[0].style.left = '0px'
+    infoGameBlocks[1].style.top =  '100px'
+    infoGameBlocks[1].style.left = '100px'
+    infoGameBlocks[2].style.top =  '0px'
+    infoGameBlocks[2].style.left = '100px'
 
-    gameBlocks[3].style.top =  '100px'
-    gameBlocks[3].style.left = '0px'
-    gameBlocks[4].style.top =  '200px'
-    gameBlocks[4].style.left = '100px'
-    gameBlocks[5].style.top =  '0px'
-    gameBlocks[5].style.left = '200px'
+    infoGameBlocks[3].style.top =  '100px'
+    infoGameBlocks[3].style.left = '0px'
+    infoGameBlocks[4].style.top =  '200px'
+    infoGameBlocks[4].style.left = '100px'
+    infoGameBlocks[5].style.top =  '0px'
+    infoGameBlocks[5].style.left = '200px'
 
-    gameBlocks[6].style.top =  '200px'
-    gameBlocks[6].style.left = '0px'
-    gameBlocks[7].style.top =  '100px'
-    gameBlocks[7].style.left = '200px'
-    gameBlocks[8].style.top =  '200px'
-    gameBlocks[8].style.left = '200px'    
+    infoGameBlocks[6].style.top =  '200px'
+    infoGameBlocks[6].style.left = '0px'
+    infoGameBlocks[7].style.top =  '100px'
+    infoGameBlocks[7].style.left = '200px'
+    infoGameBlocks[8].style.top =  '200px'
+    infoGameBlocks[8].style.left = '200px'    
 
     document.addEventListener('mouseenter', () => {
         solvePuzzle();
@@ -90,5 +145,17 @@ function renderMainPresentation(gameState) {
         solvePuzzle();
     })
 
+    let observer = new IntersectionObserver(
+      function (entries) {
+        if (entries[0].isIntersecting === true && isTimerRunning===false) {
+            startIncreasingMoves();
+            runCountdown();
+            isTimerRunning = true;
+        }
+      },
+      { threshold: [1] }
+    );
+    observer.observe(timerPresentation);
+
 }
-renderMainPresentation(gameState);
+renderMainPresentation(infoGameState);
