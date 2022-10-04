ad4e00f29dc3bf5555ace6aec1eaf3655757c03a
ajgoras <ajgoras@MacBook-Air-ajgoras.local>
2022-10-04 18:20:07 +0200

    added language rectangle to infoPage small style improvements

---
+++ src/infoPageLanguage.js
@@ -0,0 +1,21 @@
+let langButtondiv = document.getElementById('langButtondiv');
+let languageHeader = document.getElementById('language-header');
+let languageParagraph = document.getElementById('language-paragraph');
+setInterval(() => {
+    changeLanguageToPL();
+    setTimeout(() => {
+        changeLanguageToEN();
+    }, 5000);
+}, 10000);
+
+function changeLanguageToPL() {
+    langButtondiv.classList.add('PL');
+    languageHeader.innerText = 'Zmień język!';
+    languageParagraph.innerText = 'W prawym górnym rogu gry znajduje się przycisk zmiany języka. Kliknij w ten przycisk by zmienić język na Polski lub Angielski.';
+}
+
+function changeLanguageToEN() {
+    langButtondiv.classList.remove('PL');
+    languageHeader.innerText = 'Change the language!';
+    languageParagraph.innerText = 'In the upper right corner of the game there is a button which changes the language. Click on this button to change the language to Polish or English.';
+}
